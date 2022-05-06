456147041f4992e1d4a12a68fe7d18410c3ce460
Nipa <akternipa941@gmail.com>
2022-05-06 13:20:18 +0600

    delete data

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ async function run(){
             const query = {_id: ObjectId(id)};
             const item = await itemsCollection.findOne(query);
             res.send(item);
+        });
+
+        //POST
+        app.post('/inventory', async(req, res) => {
+            const newItem = req.body;
+            const result = await itemsCollection.insertOne(newItem);
+            res.send(result)
+        });
+
+        //DELETE
+        app.delete('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemsCollection.delete(query);
+            res.send(result);
         })
     }
     finally{
