1abeaeaa2ffffc65df35045e64f92db0109fdd46
Nipa <akternipa941@gmail.com>
2022-05-06 08:50:20 +0600

    service side

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.y4gbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const itemsCollection = client.db('warehouse-dress').collection('items');
+       
+        app.get('/inventory' , async(req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const item = await cursor.toArray();
+            res.send(item);
+        });
+
+        app.get('/inventory/:id' , async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Warehouse server')
+});
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
